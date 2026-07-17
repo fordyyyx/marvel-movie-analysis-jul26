6ab6914877591114dc71b20cde12227aaf80af92
Will Ford <will.ford21@imperial.ac.uk>
2026-07-17 10:35:29 +0100

    clean data and save in xlsx format

---
+++ schema/03_create_short_tables.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS marvel_no_reviews;
+
+CREATE TABLE marvel_no_reviews AS 
+SELECT 	title,
+		year,
+        type,
+		rated,
+		runtime_min,
+		genre,
+		director,
+		writer,
+		actors,
+		language,
+		country,
+		box_office_usd,
+		mcu_phase,
+		universe,
+		is_animated,
+		is_tv_series,
+		is_mcu_canon,
+		budget_usd,
+		revenue_usd,
+		episode_count,
+		season_count
+FROM marvel_master;
