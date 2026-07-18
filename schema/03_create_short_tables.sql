c3907fdae9f49ef927cc2dd4a48acd073b7de2fa
Will Ford <will.ford21@imperial.ac.uk>
2026-07-18 11:01:30 +0100

    update 03_create_short_tables

--- schema/03_create_short_tables.sql
+++ schema/03_create_short_tables.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS marvel_no_reviews;
+DROP TABLE IF EXISTS marvel_with_ids;
 
 CREATE TABLE marvel_no_reviews AS 
 SELECT 	title,
@@ -24,3 +25,37 @@ SELECT 	title,
 		episode_count,
 		season_count
 FROM marvel_master;
+
+CREATE TABLE marvel_with_ids AS
+SELECT  title,
+        year,
+        type,
+        rated,
+        runtime_min,
+        decade,
+        tmdb_id,
+        is_animated,
+        is_tv_series,
+        is_mcu_canon,
+        budget_usd,
+        revenue_usd,
+        episode_count,
+        season_count
+FROM marvel_master;
+
+WITH marvel_cast_actors AS (
+SELECT  actor_name,
+		CHARACTER,
+		tmdb_id
+FROM marvel_cast)
+
+
+SELECT  mwi.title,
+		mwi.YEAR,
+		mwi.TYPE,
+		mwi.runtime_min,
+		mwi.revenue_usd,
+		mca.actor_name,
+		mca.character	
+FROM marvel_with_ids mwi
+LEFT JOIN marvel_cast_actors mca ON mwi.tmdb_id = mca.tmdb_id;
